b0ce3328b87370d5e004f91b375d1a5cde3bdb65
keithvis <keith_visvardis@yahoo.com>
2022-12-17 01:05:39 +0200

    tasks 1 2 3 4

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
         Welcome to React
         </p>
@@ -17,7 +14,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }
